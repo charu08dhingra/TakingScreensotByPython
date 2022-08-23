a2e04b068fe106f2fc34b8911b93c2a2ae22a0b8
Charu Dhingra <91649001+charu08dhingra@users.noreply.github.com>
2022-08-23 11:12:24 +0530

    Update Screenshot.py

--- Screenshot.py
+++ Screenshot.py
@@ -1,5 +1,8 @@
 import pyscreenshot
 
+#by this you can change the dimensions of the page 
 image=pyscreenshot.grab()
+# to show the screenshot
 image.show()
+# to save the page
 image.save("hy.png")
